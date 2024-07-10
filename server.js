989644898ba1fbd468db7965502b0afe96f91413
Mirza Korac <mirzakorac@Mirzas-Mac-mini.local>
2024-07-10 18:57:02 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require('dotenv');
+const cors = require("cors")
+const path = require("path")
+
+
+
+
+
+dotenv.config()
+
+
+if(process.env.NODE_ENV === "local") {
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }))
+
+
+} else{
+  app.use(cors({
+    credentials: true
+  }))
+}
+
+
+if(process.env.NODE_ENV === "production") {
+ app.use(express.static(path.join(__dirname, "./frontend/dist")))
+ app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/", "frontend", "dist", "index.html"))
+ })
+}
+
+
+
+
+const dbConnect = async () => {
+  try {
+    if(process.env.NODE_ENV === "local") {
+      await mongoose.connect(process.env.LOCAL_DB_URI)
+      console.log("Local Database is connected..");
+    } else {
+      await mongoose.connect(process.env.MONGODB_URI)
+      console.log("Production Database is connected..");
+    }
+  } catch (error) {
+     console.log("Database connection failed.");
+  }
+}
+
+dbConnect()
+
+
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`Server is runing on port ${PORT}..`))
